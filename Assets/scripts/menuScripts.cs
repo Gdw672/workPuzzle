e0cf0c50735320fcb4ca59d0653443e950913dde
Gdw672 <van_der_linde90@mail.ru>
2022-05-10 20:00:52 +0300

    +ui

---
+++ Assets/scripts/menuScripts.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menuScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+   [SerializeField] public void loadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    [SerializeField] public void loadMenuFromStore()
+    {
+        SceneManager.LoadScene(0);
+    }
+   
+}
